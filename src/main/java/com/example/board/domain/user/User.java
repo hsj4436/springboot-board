e25b8efd946a1e1ba8b24598d7db2ccf7a55aa32
hsj4436 <hsj4436@naver.com>
2022-12-26 01:38:48 +0900

    feat: Builder 패턴 통한 User 생성자 추가

--- src/main/java/com/example/board/domain/user/User.java
+++ src/main/java/com/example/board/domain/user/User.java
@@ -1,11 +1,16 @@
 package com.example.board.domain.user;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
-@Entity
 @Table(name = "USERS")
+@Getter
+@NoArgsConstructor
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +31,12 @@ public class User {
 
     @CreationTimestamp
     private Timestamp createDate;
+
+    @Builder
+    public User(String userName, String password, String email, Role role) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
 }
